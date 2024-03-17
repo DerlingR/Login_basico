2544ddea049e5fa562b2e98bb64457e076531d81
Derling <derlingmejia07@outlook.com>
2024-03-17 17:26:20 -0400

    Evidencias del proyecto

--- login.php
+++ login.php
@@ -7,18 +7,20 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = $_POST['username'];
     $password = $_POST['password'];
 
-    // Conectar a la base de datos
-    $servername = "localhost";
-    $username_db = "root";
-    $password_db = "DerlingM2001";
-    $dbname = "loginbasico";
+    // Incluir el archivo de configuración
+    include 'config.php';
 
+    // Conectar a la base de datos utilizando las variables definidas en config.php
     $conn = new mysqli($servername, $username_db, $password_db, $dbname);
 
     // Verificar la conexión
     if ($conn->connect_error) {
         die("Conexión fallida: " . $conn->connect_error);
     }
+
+    // Escapar los valores de los campos de usuario y contraseña para evitar inyección SQL
+    $username = mysqli_real_escape_string($conn, $username);
+    $password = mysqli_real_escape_string($conn, $password);
 
     // Consultar la base de datos para verificar las credenciales
     $sql = "SELECT * FROM usuarios WHERE nombre='$username' AND password='$password'";
